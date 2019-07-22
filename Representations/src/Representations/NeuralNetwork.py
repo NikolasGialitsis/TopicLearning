ad3d6502008a612f70e79fcd1e641626ac297343
NikolasGialitsis <nikolasyal@gmail.com>
2019-07-22 22:34:31 +0300

    @george edit - dense

--- Representations/src/Representations/NeuralNetwork.py
+++ Representations/src/Representations/NeuralNetwork.py
@@ -2,7 +2,8 @@
 import numpy as np
 import sys
 
-from keras.layers import LSTM, Dense, TimeDistributed,Activation,Input
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.layers import Dropout, LSTM, Dense, TimeDistributed,Activation,Input
 from ast import literal_eval
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import StratifiedKFold
@@ -34,22 +35,22 @@ from sklearn.dummy import  DummyClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import r2_score
 from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier
 
 import tensorflow as tf
 
 
-text_file = open("/home/superuser/SequenceEncoding/Representations/prob_repr.dat", "r")
+text_file = open("prob_repr.dat", "r")
 instances_concat = text_file.readlines()
 num_steps = -1
 instances = []
 for instance in instances_concat:
     vec = literal_eval(instance)
     num_steps = len(vec)
-    input_dim = len(vec[0])
     instances.append(vec)
 
 text_file.close()
-labels_file = open("/home/superuser/SequenceEncoding/Representations/prob_labels.dat", "r")
+labels_file = open("prob_labels.dat", "r")
 labels  = labels_file.readlines()
 labels_file.close()
 
@@ -59,20 +60,23 @@ from sklearn.preprocessing import OneHotEncoder
 label_encoder = LabelEncoder()
 integer_encoded = label_encoder.fit_transform(labels)
 labels = integer_encoded
-print 'Instances num = '+ str(len(instances))
-print 'Labels num = ' + str(len(labels))
+print('Instances num = '+ str(len(instances)))
+print('Labels num = ' + str(len(labels)))
 def learning_model(): #Neural Network Architecture#
     global input_dim
     global num_steps
-    global input
+    global input_shape
+    
     model = Sequential()
-    print 'dim '+str(input_dim)
-    print 'steps '+str(num_steps)
-    input_shape=(num_steps,input_dim)
+    #print('dim '+str(input_dim))
+    #print('steps '+str(num_steps))
+    #input_shape=(num_steps,input_dim)
     #model.add(LSTM(10, input_shape=input_shape,dropout=0.2, recurrent_dropout=0.2))
-    model.add(Dense(10,input_shape=input_shape))
-    model.add(Dense(10))
-    model.add(Dense(2,activation='softmax'))
+    
+    model.add(Dense(100))
+    model.add(Dropout(0.2))
+    model.add(Dense(100))
+    model.add(Dense(1, activation='softmax'))
     model.compile(loss="binary_crossentropy", optimizer="adam",metrics=['accuracy'])
     return model
 
@@ -93,7 +97,7 @@ def learning_model(): #Neural Network Architecture#
 
 # fix random seed for reproducibility
 
-epochs = 1
+epochs = 100
 seed = 111
 batches = 64
 
@@ -104,28 +108,49 @@ skf = StratifiedKFold(n_splits=10)
 from sklearn.metrics import f1_score
 instances = np.array(instances)
 labels = np.array(labels)
+print("+++ Starting fold...")
 for train_index, test_index in skf.split(instances, labels):
     #print("TRAIN:", train_index, "TEST:", test_index)
-    x_train, x_test = instances[train_index], instances[test_index]
-    y_train, y_test = labels[train_index], labels[test_index]
-    print '---- XTRAIN ----\n'+str(x_train[0])
-    print '---- YTRAIN ----\n'+str(y_train[0])
+    print("+ Flattening instances...")
+    flattenedInstances = np.array([np.ndarray.flatten(xVec) for xVec in instances])
+    print("+ Flattening instances... Done.")
+    
+    x_train, x_test = np.array(flattenedInstances[train_index]), np.array(flattenedInstances[test_index])
+    y_train, y_test = np.array(labels[train_index]), np.array(labels[test_index])
+    print('---- XTRAIN 1st instance ----\n'+str(x_train[0]))
+    print('---- YTRAIN 1st instance  ----\n'+str(y_train[0]))
 
-    non_linear_model = learning_model()
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=0,patience=5)
-    x_train = np.array(x_train)
-    x_test = np.array(x_test)
-    y_train = np.array(y_train)
-    y_test = np.array(y_test)
-    non_linear_model.fit(x =x_train,y=y_train,epochs=epochs, batch_size=batches,validation_data=(x_test,y_test),callbacks=[es])
+
+    input_dim = x_train.shape[1]
+    print("Shape:" + str(x_train.shape))
+    
+    non_linear_model = KerasClassifier(build_fn=learning_model, epochs=epochs)
+    
+    #non_linear_model = DecisionTreeClassifier()
+    
     # Final evaluation of the model
-    print non_linear_model.summary()
-    y_pred =non_linear_model.predict(x_test)
+    # Neural Network
+    #print(non_linear_model.summary())
+    #non_linear_model.fit(X=x_train,y=y_train)
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    print("+ Fitting model...")
+    es = EarlyStopping(monitor='loss', mode='min', verbose=0,patience=5)
+    non_linear_model.fit(x =x_train,y=y_train,validation_data=(x_test,y_test),callbacks=[es])
+    print("+ Fitting model... Done.")
+    
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+    print("+ Predicting...")    
+    y_pred = non_linear_model.predict(x_test)
+    print("+ Predicting... Done.")
 
+    #non_linear_model.fit(x =x_train,y=y_train,epochs=epochs, batch_size=batches,validation_data=(x_test,y_test),callbacks=[es])
+    
     #print set(y_test) - set(y_pred)
-    print ('f1 score macro ' + str(f1_score(y_test, y_pred, average='macro')))
-    print ('f1 score micro ' + str(f1_score(y_test, y_pred, average='micro')))
-    print ('Confusion matrix:\n' + str(confusion_matrix(y_test, y_pred)))
+    print(('f1 score macro ' + str(f1_score(y_test, y_pred, average='macro'))))
+    print(('f1 score micro ' + str(f1_score(y_test, y_pred, average='micro'))))
+    print(('Confusion matrix:\n' + str(confusion_matrix(y_test, y_pred))))
 
 
 #(x_train, x_test,y_train,y_test) = train_test_split(instances,labels, test_size=0.2, random_state=seed)
