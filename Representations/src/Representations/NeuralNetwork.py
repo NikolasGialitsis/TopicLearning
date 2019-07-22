Changed NN metrics to FF and softmax Fixed TFIDF representations [done] Run TFIDF representation on multiling english dataset[training model] check tokenizations in probabilistic repr [to do] check why last layer needs to have 2 neurons and softmax [to do]

Representations/src/Representations/NeuralNetwork.py
@@ -34,7 +34,7 @@ from sklearn.decomposition import PCA
 import tensorflow as tf
 
 
-text_file = open("/home/superuser/SequenceEncoding/Representations/tfidf_repr.dat", "r")
+text_file = open("/home/superuser/SequenceEncoding/Representations/prob_repr.dat", "r")
 instances_concat = text_file.readlines()
 num_steps = -1
 instances = []
@@ -45,7 +45,7 @@ for instance in instances_concat:
     instances.append(vec)
 
 text_file.close()
-labels_file = open("/home/superuser/SequenceEncoding/Representations/tfidf_labels.dat", "r")
+labels_file = open("/home/superuser/SequenceEncoding/Representations/prob_labels.dat", "r")
 labels  = labels_file.readlines()
 labels_file.close()
 
@@ -57,6 +57,21 @@ integer_encoded = label_encoder.fit_transform(labels)
 labels = integer_encoded
 
 
+def learning_model_NN(): #Neural Network Architecture#
+    global input_dim
+    global num_steps
+    global input
+    model = Sequential()
+    print 'dim '+str(input_dim)
+    print 'steps '+str(num_steps)
+    input_shape=(num_steps,input_dim)
+    model.add(LSTM(200, input_shape=input_shape,dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(200))
+    model.add(Dense(2,activation='softmax'))
+    model.compile(loss="binary_crossentropy", optimizer="adam",metrics=['accuracy'])
+    print model.summary()
+    return model
+
 def learning_model(): #Neural Network Architecture#
     global input_dim
     global num_steps
@@ -67,22 +82,22 @@ def learning_model(): #Neural Network Architecture#
     input_shape=(num_steps,input_dim)
     model.add(LSTM(200, input_shape=input_shape,dropout=0.2, recurrent_dropout=0.2))
     model.add(Dense(200))
-    model.add(Dense(1,activation='softmax'))
+    model.add(Dense(2,activation='softmax'))
     model.compile(loss="binary_crossentropy", optimizer="adam",metrics=['accuracy'])
     print model.summary()
     return model
 
 # fix random seed for reproducibility
 
-epochs = 2
-seed = 40
+epochs = 5
+seed = 111
 batches = 64
-
 
 #partition dataset
 (x_train, x_test,y_train,y_test) = train_test_split(instances,labels, test_size=0.2, random_state=seed)
-non_linear_model = learning_model()
-es = EarlyStopping(monitor='loss', mode='min', verbose=0)
+# non_linear_model = learning_model()
+non_linear_model = DummyClassifier()
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=0,patience=5)
 x_train = np.array(x_train)
 x_test = np.array(x_test)
 y_train = np.array(y_train)