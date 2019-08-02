Major fixes in probabilistic representation. Tried averaging,maximizing,ordering vectors.
Signed-off-by: NikolasGialitsis <sdi1400027@di.uoa.gr>

Representations/src/Representations/Predict_Label.py
@@ -49,7 +49,7 @@ classifiers = [
     #SVC(kernel="rbf", C=0.025, probability=True),
     #NuSVC(probability=True),
     # QuadraticDiscriminantAnalysis()
-    #Sequential(),
+    #?Sequential(),
     DecisionTreeClassifier(),
     RandomForestClassifier(),
     AdaBoostClassifier(),
@@ -95,7 +95,7 @@ def main():
     labels = np.array(labels)
     name = classifiers[1].__class__.__name__
 
-    loaded_model = pickle.load(open("TrainedModels/"+name+".pickle", "rb"))
+    loaded_model = pickle.load(open(repr+"_TrainedModels/"+name+".pickle", "rb"))
     model_n_features = 0
     if name is not Sequential:
         model_n_features = loaded_model.n_features_/(topic_num)
@@ -126,13 +126,13 @@ def main():
         # load model from file
 
         print(name)
-        loaded_model = pickle.load(open("TrainedModels/"+name+".pickle", "rb"))
+        loaded_model = pickle.load(open(repr+"_TrainedModels/"+name+".pickle", "rb"))
         # make predictions for test data
         print('Make predictions...')
         y_pred = loaded_model.predict(x_test)
         print('shape of predicted labels :'+str( y_pred.shape))
         print y_pred
-        pickle.dump(np.ndarray(shape=(y_pred.shape),dtype=int,buffer=y_pred), open("results/"+name + ".pickle", "wb"))
+        pickle.dump(np.ndarray(shape=(y_pred.shape),dtype=int,buffer=y_pred), open("results/"+repr+"_"+name + ".pickle", "wb"))
         print('...Done\n')
         micro_sum = f1_score(y_test,y_pred,average="micro")
         macro_sum = f1_score(y_test,y_pred,average="macro")

Representations/src/Representations/Train_Model.py
@@ -193,13 +193,13 @@ def main():
             if(name == 'Sequential'):
                 x_train = np.array(instances)
                 x_train = x_train[:, :num_steps, :]
-                non_linear_model.fit(x_train, y_train,batch_size=32,epochs=10,shuffle=False)
+                non_linear_model.fit(x_train, y_train,batch_size=64,epochs=2,shuffle=False)
             else:
                 flattenedInstances = np.array([np.ndarray.flatten(xVec) for xVec in instances])
                 x_train = np.array(flattenedInstances)
                 non_linear_model.fit(x_train, y_train)
             # save model to file
-            pickle.dump(non_linear_model, open("TrainedModels/"+name + ".pickle", "wb"))
+            pickle.dump(non_linear_model, open(repr+"_TrainedModels/"+name + ".pickle", "wb"))
 if __name__ == '__main__':
     print 'main init'
     main()